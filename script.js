7245205ed2d64dfe2a521ce70a2df45a3544ae38
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-07-27 21:35:35 -0400

    Condicionlaes if

--- script.js
+++ script.js
@@ -65,3 +65,66 @@ document.write(num1==num2)
 document.write("<br>")
 document.write(num1!=num2)
 
+//Estrucuras de Control en JavaScript
+//1. TOMA DE DESICIONES
+//1.1. IF
+//1.2. SWITCH
+
+//1.1. Condicionales IF
+//if (expresion){
+    //Accion si se cumple la condicion
+//}
+//Ejercicio 3. Comparar entre los 2 numeros cual es el mayor
+document.write("<br>")
+document.write("Ejercicio 3. Comparar entre los 2 numeros cual es el mayor")
+document.write("<br>")
+if (num1>num2){
+    document.write("El numero ",num1, " es mayor que ",num2)
+}else{
+    document.write("El numero ",num2, " es mayor que ",num1)
+}
+//Ejercicio 4. Crear un menu para una calculadora simple
+document.write("<br>")
+document.write("Ejercicio 4. Crear un menu para una calculadora simple")
+document.write("<br>")
+document.write("Seleccione la operacion que desea realizar")
+document.write("<br>")
+document.write("1. Sumar")
+document.write("<br>")
+document.write("2. Restar")
+document.write("<br>")
+document.write("3. Multiplicar")
+document.write("<br>")
+document.write("4. Dividir")
+document.write("<br>")
+seleccion=parseInt(prompt("Ingrese la operacion, 1. Sumar, 2. Restar, 3. Multiplicar, 4. Dividir "))
+var numm1=parseFloat(prompt("Ingrese un numero"))
+var numm2=parseFloat(prompt("Ingrese un numero"))
+if (seleccion==1){
+    var suma=numm1+numm2
+    document.write("<br>")
+    document.write("La suma de ",numm1,"+",numm2,"= ",suma)
+}
+else if(seleccion==2){
+    var resta=numm1-numm2
+    document.write("<br>")
+    document.write("La resta de ",numm1,"-",numm2,"= ",resta)
+}
+else if(seleccion==3){
+    var multiplicar=numm1*numm2
+    document.write("<br>")
+    document.write("La multiplicacion de ",numm1,"x",numm2,"= ",multiplicar)
+}
+else if(seleccion==4){
+    var dividir=numm1/numm2
+    document.write("<br>")
+    document.write("La division de ",numm1,"/",numm2,"= ", dividir)
+}
+else{
+    alert("Error operacion no valida")
+}
+
+
+
+//2. BUCLES
+

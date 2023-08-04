c9c790a3a58854c54a819c4ad386de70bce67975
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-08-03 21:55:32 -0400

    CLASS ID JAVASCRIPT

--- tablademultiplicar.js
+++ tablademultiplicar.js
@@ -1,1 +1,21 @@
 //Ejercicio 10. Crear un sistema para mostrar la tabla de multiplicar de cualquier digito ingresado por teclado
+//var numero=prompt("Ingrese la tabla de multiplicar que desea mostrar")
+document.write("<br>")
+for(var i=1;i<=10;i++){
+    document.write("<br>")
+    document.write(numero+"x"+i+"= "+ numero*i)
+}
+//Ejercicio 11. Crear un boton que active una funcion para mostrar la tabla de m multiplicar
+//Funciones en Javascript
+function tablaMultiplicar(numero){
+    var n=prompt("Ingrese la tabla de multiplicar que desea mostrar")
+    document.write("<br>")
+    for(var i=1;i<=10;i++){
+        document.write("<br>")
+        document.write(n+"x"+i+"= "+ n*i)
+    }
+}
+
+var n=prompt("Ingrese la tabla de multiplicar que desea mostrar")
+document.getElementById("tabla").onclick=tablaMultiplicar()
+document.getElementsByClassName("tabla-C").onclick=tablaMultiplicar()

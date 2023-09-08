9faec99aca6aa7da9fd73ed550348d205ec0fc66
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-09-07 22:04:54 -0400

    Confirmacion de Venta

--- idclases.js
+++ idclases.js
@@ -3,6 +3,7 @@ const listacarrito=document.getElementById('lista-carrito')
 const contadorAgregados=document.getElementById('contador');
 const totalHTMl=document.getElementById('total');
 
+
 //let totalPagar=0;
 let contador=0;
 //Crear la lista del carrito
@@ -59,4 +60,50 @@ boton.addEventListener('click',(event)=>{
         });
     });
 }
+
+const botonConfirmarVenta=getElementById('confirmar-venta');
+const formularioEmergente=getElementById('formulario-emergente');
+
+//Evento para confirmar la venta
+botonConfirmarVenta.addEventListener('click',()=>{
+    formularioEmergente.style.display='block';
+});
+//Cierre de el formulario si se confirma la compra
+const formularioCompra=document.getElementById('formulario-compra');
+formularioCompra.addEventListener('submit',(event)=>{
+    //Actualizar el estado de la compra - detalle (factura)
+    event.preventDefault();//Evita que el formulario se envie
+    //Obtner los valores del formulario
+    const nombreCliente=document.getElementById('nombre').value;
+    const emailCliente=document.getElementById('email').value;
+    const celularCliente=document.getElementById('phone').value;
+    //Obtener el contenido del carrito de compras
+    const listaCarritoCompras=document.getElementById('lista-carrito').innerHTML;
+    //Obtener el precio total
+    const precioTotal=document.getElementById('total').textContent;
+    //Creacion de la factura con el resumen de la compra
+    const resumenCompra=`
+        <h3>Resumen Compra</h3>
+        <p>Nombre Cliente: ${nombreCliente} </p>
+        <p>Correo: ${emailCliente} </p>
+        <p>Celular: ${celularClienteCliente} </p>
+        <p>Productos Comprados</p>
+        <ul>
+            ${listaCarritoCompras}
+        </ul>
+        <p>Precio Total: ${precioTotal}</p>
+        <br>
+        <p>Gracias por su compra!!!</p>
+    `;
+
+    //Abrir la ventana emergente con el resumen de la compra
+    const factura=window.open('','_blank', 'width=300, height=300');
+    factura.document.open();
+
+    factura.document.write(resumenCompra);
+    factura.document.close();
+
+    //Cerrar el formulario emergente
+    formularioEmergente.style.display='none';
+});
 

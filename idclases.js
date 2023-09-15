8cc075218884d8213e8a45e29082d574493f1402
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-09-14 20:52:23 -0400

    Pantalla emergente

--- idclases.js
+++ idclases.js
@@ -1,109 +1,92 @@
-const botonesAgregar=document.querySelectorAll('.agregar-carrito');
-const listacarrito=document.getElementById('lista-carrito')
-const contadorAgregados=document.getElementById('contador');
-const totalHTMl=document.getElementById('total');
+const botonesAgregar = document.querySelectorAll('.agregar-carrito');
+const listacarrito = document.getElementById('lista-carrito');
+const contadorAgregados = document.getElementById('contador');
+const totalHTMl = document.getElementById('total');
 
+let contador = 0;
+const carrito = [];
 
-//let totalPagar=0;
-let contador=0;
-//Crear la lista del carrito
-const carrito=[];
-
-botonesAgregar.forEach((boton,index)=>
-{
-    boton.addEventListener('click',()=>{
+botonesAgregar.forEach((boton, index) => {
+    boton.addEventListener('click', () => {
         contador++;
-        const cantidadInput=boton.previousElementSibling;
-        const cantidad=parseInt(cantidadInput.value);
-        if(!isNaN (cantidad)&& cantidad>0){
-            const nombre=boton.dataset.nombre;
-            const precio=boton.dataset.precio;
-            const producto={
-                nombre:nombre,
-                precio:precio,
-                cantidad:cantidad
+        const cantidadInput = boton.previousElementSibling;
+        const cantidad = parseInt(cantidadInput.value);
+        if (!isNaN(cantidad) && cantidad > 0) {
+            const nombre = boton.dataset.nombre;
+            const precio = boton.dataset.precio;
+            const producto = {
+                nombre: nombre,
+                precio: precio,
+                cantidad: cantidad
             };
             carrito.push(producto);
-                
-            contador+=cantidad;
-            contadorAgregados.textContent=contador;
+
+            contador += cantidad;
+            contadorAgregados.textContent = contador;
         }
-         mostrarcarrito();
-    }); 
+        mostrarcarrito();
+    });
 });
 
-//Funcion para mostrar el carrito
-function mostrarcarrito(){
-    listacarrito.innerHTML='';
-    //Variable para controlar el total
-    let totalPagar=0;
+function mostrarcarrito() {
+    listacarrito.innerHTML = '';
+    let totalPagar = 0;
 
-    carrito.forEach((producto,index)=>
-    {
-        const elementoLista=document.createElement('li');
-        elementoLista.innerHTML=`${producto.nombre} - Precio: ${producto.precio}Bs <button class="eliminar-producto" data-index="${index} ">Eliminar</button> <br>`;
-        listacarrito.appendChild(elementoLista);   
-        totalPagar+=producto.cantidad*producto.precio;
+    carrito.forEach((producto, index) => {
+        const elementoLista = document.createElement('li');
+        elementoLista.innerHTML = `${producto.nombre} - Precio: ${producto.precio} Bs <button class="eliminar-producto" data-index="${index}">Eliminar</button> <br>`;
+        listacarrito.appendChild(elementoLista);
+        totalPagar += producto.cantidad * producto.precio;
     });
-    totalHTMl.textContent=totalPagar.toFixed(2);
+    totalHTMl.textContent = totalPagar.toFixed(2);
 
+    const botonEliminar = document.querySelectorAll('.eliminar-producto');
 
-const botonEliminar=document.querySelectorAll('.eliminar-producto');
-
-botonEliminar.forEach((boton)=>{
-boton.addEventListener('click',(event)=>{
-        const index=event.target.dataset.index;
-        carrito.splice(index, 1);
-        contador--;
-        contadorAgregados.textContent=contador;
-        mostrarcarrito();
+    botonEliminar.forEach((boton) => {
+        boton.addEventListener('click', (event) => {
+            const index = event.target.dataset.index;
+            carrito.splice(index, 1);
+            contador--;
+            contadorAgregados.textContent = contador;
+            mostrarcarrito();
         });
     });
 }
 
-const botonConfirmarVenta=getElementById('confirmar-venta');
-const formularioEmergente=getElementById('formulario-emergente');
+const botonConfirmarVenta = document.getElementById('confirmar-venta');
+const formularioEmergente = document.getElementById('formulario-emergente');
+const formularioCompra = document.getElementById('formulario-compra');
 
-//Evento para confirmar la venta
-botonConfirmarVenta.addEventListener('click',()=>{
-    formularioEmergente.style.display='block';
+botonConfirmarVenta.addEventListener('click', () => {
+    formularioEmergente.style.display = 'block';
 });
-//Cierre de el formulario si se confirma la compra
-const formularioCompra=document.getElementById('formulario-compra');
-formularioCompra.addEventListener('submit',(event)=>{
-    //Actualizar el estado de la compra - detalle (factura)
-    event.preventDefault();//Evita que el formulario se envie
-    //Obtner los valores del formulario
-    const nombreCliente=document.getElementById('nombre').value;
-    const emailCliente=document.getElementById('email').value;
-    const celularCliente=document.getElementById('phone').value;
-    //Obtener el contenido del carrito de compras
-    const listaCarritoCompras=document.getElementById('lista-carrito').innerHTML;
-    //Obtener el precio total
-    const precioTotal=document.getElementById('total').textContent;
-    //Creacion de la factura con el resumen de la compra
-    const resumenCompra=`
-        <h3>Resumen Compra</h3>
-        <p>Nombre Cliente: ${nombreCliente} </p>
-        <p>Correo: ${emailCliente} </p>
-        <p>Celular: ${celularClienteCliente} </p>
-        <p>Productos Comprados</p>
+
+formularioCompra.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nombreCliente = document.getElementById('nombre').value;
+    const emailCliente = document.getElementById('email').value;
+
+    const listaCarrito = document.getElementById('lista-carrito').innerHTML;
+
+    const precioTotal = document.getElementById('total').textContent;
+
+    const resumenCompra = `
+        <h3>Resumen de Compra</h3>
+        <p>Nombre del Cliente: ${nombreCliente}</p>
+        <p>Correo Electrónico: ${emailCliente}</p>
+        <p>Productos Comprados:</p>
         <ul>
-            ${listaCarritoCompras}
+            ${listaCarrito}
         </ul>
-        <p>Precio Total: ${precioTotal}</p>
-        <br>
-        <p>Gracias por su compra!!!</p>
+        <p>Precio Total: Bs ${precioTotal}</p>
+        <p>¡Gracias por su compra!</p>
     `;
 
-    //Abrir la ventana emergente con el resumen de la compra
-    const factura=window.open('','_blank', 'width=300, height=300');
-    factura.document.open();
+    const nuevaVentana = window.open('', '_blank', 'width=400,height=400');
+    nuevaVentana.document.open();
+    nuevaVentana.document.write(resumenCompra);
+    nuevaVentana.document.close();
 
-    factura.document.write(resumenCompra);
-    factura.document.close();
-
-    //Cerrar el formulario emergente
-    formularioEmergente.style.display='none';
+    formularioEmergente.style.display = 'none';
 });
-
